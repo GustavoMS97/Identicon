7f02e538a606b5cbe320d535dd845beb2d83e1b0
Gustavo Santos <gustavoms97@hotmail.com>
2020-05-15 15:09:22 -0300

    Finalizado o codigo para gerar imagens aleatorias, assim como o github/gitlab geram para cada usuário.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,11 +3,81 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g, b]
+  def save_image(image_file, input) do
+    File.write("#{input}.png", image_file)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    # EGD is a ERLang library to create images
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+        {top_left, bottom_right}
+      end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter(grid, fn {code, _index} ->
+        # Calculates the rest/remainder
+        # Different from JS, elixir wont understand 0 as false, so we need the '=='
+        rem(code, 2) == 0
+      end)
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      # Enum.map works like every other language (JS)
+      # To pass a reference to a function use & at the beggining, and the '/1'
+      # specifically says it wants the one with one argument
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      # Loops through the list, creating tuples for each element, putting the item and its index.
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    # [145, 46, 200]
+    [first, second | _tail] = row
+    # [145, 46, 200, 46, 145] -> use ++ to append arrays
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    # Create new struct with everything from 'image'
+    # and add a tuple with the colors data
+
+    # NEVER modify data, always create new data.
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
